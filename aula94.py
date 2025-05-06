c5e6dbcc17f59f5415a94a0a09c3dd65bec67ccb
Rafaeljobf <rafaf1604@gmail.com>
2025-05-05 21:29:50 -0300

    aula94 commit

--- aula94.py
+++ aula94.py
@@ -1,8 +1,10 @@
 # try, except, else e finally
 try:
     print('ABRIR ARQUIVO')
-    8/0
+    # 8/0
 except ZeroDivisionError:
     print('DIVIDIU POR ZERO')
+else:
+    print("SEM ERROS")
 finally:
     print('FECHAR ARQUIVO')
